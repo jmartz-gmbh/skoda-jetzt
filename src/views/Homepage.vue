58c8951e48ece2b15058ebcf80b8d683ea3076bd
Jonathan Martz <git@jmartz.de>
2021-12-08 19:53:46 +0100

    initial commit

---
+++ src/views/Homepage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="vp-homepage">
+      <div class="grid grid-cols-4">
+        <div class="col-span-4 md:col-span-2">
+          Homepage
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Homepage",
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    this.$store.commit('seo-prepare', {
+      title: 'Startseite',
+      description: `Willkommen auf der neuen Webseite.`
+    });
+  },
+}
+</script>
